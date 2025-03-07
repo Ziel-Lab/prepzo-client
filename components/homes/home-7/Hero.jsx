813977114283fee2db1d2763fd63204e14415c86
KirtiPriya07 <kirtipriya64@gmail.com>
2025-03-07 18:37:15 +0530

    removed google icon, need to add LiveKit voice

--- components/homes/home-7/Hero.jsx
+++ components/homes/home-7/Hero.jsx
@@ -156,12 +156,6 @@ export default function Hero() {
                       href="#"
                       className="btn btn-md xl:btn-lg btn-alt-dark border-dark px-3 lg:px-5 fw-bold contrast-shadow-sm hover:contrast-shadow"
                     >
-                      <Image
-                        alt="Google Icon"
-                        src="/assets/images/template/google-icon.svg"
-                        width="20"
-                        height="21"
-                      />
                       <span>Talk to Prepzo</span>
                     </a>
                   </div>
