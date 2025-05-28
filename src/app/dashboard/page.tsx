83d19ec2c46c4b958dbd19d7cb1f7bbdb0e13007
Kirti Priya <kirtipriya64@gmail.com>
2025-05-28 11:10:29 +0530

    redirect fixed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -39,7 +39,7 @@ const DashboardPage = () => {
 
       if (authError || !user) {
         console.error("User not authenticated or error fetching user. Redirecting to login.", authError);
-        window.location.href = '/'; // Redirect to your login page
+        window.location.href = '/auth/sign-up'; // Redirect to your login page
         return; // Stop further execution in this effect
       }
 
