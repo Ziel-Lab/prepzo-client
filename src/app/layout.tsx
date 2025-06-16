e98dfde03c7165e33cece0233548fbee1aa88e63
Kushagra Gangwar <kgang6434@gmail.com>
2025-06-16 12:03:43 +0530

    updated meta tags homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,34 @@ import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] }); 
 
 export const metadata: Metadata = {
-  title: "Prepzo", 
-  description: "Your AI voice assistant for personalized career guidance.", // Adjust description
-};
-
+  title: "Prepzo.ai | AI Career Coach & Job Search Assistant",
+  description:
+    "Prepzo is an AI voice assistant that helps you overcome career challenges with personalized strategies for job search, skill development, and career growth. Try a free demo—no credit card required.",
+  openGraph: {
+    title: "Prepzo.ai | AI Career Coach & Job Search Assistant",
+    description:
+      "Prepzo is an AI voice assistant that helps you overcome career challenges with personalized strategies for job search, skill development, and career growth. Try a free demo—no credit card required.",
+    url: "https://prepzo.ai/",
+    siteName: "Prepzo.ai",
+    images: [
+      {
+        url: "https://prepzo.ai/og.jpeg", 
+        width: 1200,
+        height: 630,
+        alt: "Prepzo.ai preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Prepzo.ai | AI Career Coach & Job Search Assistant",
+    description:
+      "Prepzo is an AI voice assistant that helps you overcome career challenges with personalized strategies for job search, skill development, and career growth. Try a free demo—no credit card required.",
+    images: ["https://prepzo.ai/og-image.png"], 
+  },
+}; 
 export default function RootLayout({
   children,
 }: Readonly<{
