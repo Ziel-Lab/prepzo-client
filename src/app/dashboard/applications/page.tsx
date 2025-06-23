a8ab5151a970da0409d880be96e4424cc0c046a1
Kushagra Gangwar <kgang6434@gmail.com>
2025-06-23 16:51:30 +0530

    masked application page

--- src/app/dashboard/applications/page.tsx
+++ src/app/dashboard/applications/page.tsx
@@ -5,7 +5,7 @@ import ApplicationsTable, { Filters } from "@/components/dashboard/applications/
 import ApplicationsFilters from "@/components/dashboard/applications/ApplicationsFilters";
 import ApplicationsStats from "@/components/dashboard/applications/ApplicationsStats";
 import NewApplicationDialog from "@/components/dashboard/applications/NewApplicationDialog";
-
+import BlurOverlay from "@/components/dashboard/blurrEffect";
 
 // Re-use the primitive/loose filter map used by AdvancedFilters
 type Primitive = string | number | boolean;
@@ -26,26 +26,31 @@ const Applications = () => {
 
   return (
     <DashboardLayout>
+      <BlurOverlay />
       <div className="space-y-6">
         <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
           <div>
             <h1 className="text-3xl font-bold text-gray-900">Applications</h1>
             <p className="text-gray-600 mt-1">Track your job applications and progress</p>
           </div>
+          <NewApplicationDialog />
         </div>
-
-        {/* Content Locked State */}
-        <div className="flex flex-col items-center justify-center min-h-[400px] space-y-4">
-          <div className="w-16 h-16 text-[#4A6163]">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-              <path fillRule="evenodd" d="M12 1.5a5.25 5.25 0 00-5.25 5.25v3a3 3 0 00-3 3v6.75a3 3 0 003 3h10.5a3 3 0 003-3v-6.75a3 3 0 00-3-3v-3c0-2.9-2.35-5.25-5.25-5.25zm3.75 8.25v-3a3.75 3.75 0 10-7.5 0v3h7.5z" clipRule="evenodd" />
-            </svg>
+        
+        {/* <ApplicationsStats /> */}
+        
+        <div className="flex flex-col lg:flex-row gap-4">
+          <div className="flex-1">
+            <ApplicationsFilters onFiltersChange={handleFiltersChange} />
           </div>
-          <h2 className="text-2xl font-semibold text-gray-900">Content Locked</h2>
-          <p className="text-gray-600 text-center max-w-md">
-            We are building this feature for you. Please check back soon!
-          </p>
+          <div className="lg:w-auto">
+            {/* <AdvancedFilters 
+              onFiltersChange={handleAdvancedFiltersChange}
+              activeFilters={advancedFilters}
+            /> */}
+          </div>
         </div>
+        
+        <ApplicationsTable filters={combinedFilters} />
       </div>
     </DashboardLayout>
   );
