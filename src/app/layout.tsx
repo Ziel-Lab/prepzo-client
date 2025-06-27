bb0761d21294e481b9280a8e9f6f816ff1c41171
Kushagra Gangwar <kgang6434@gmail.com>
2025-06-27 16:10:27 +0530

    added g-tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -145,6 +145,25 @@ export default function RootLayout({
         </ClientProviders>
         <Analytics />
         
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0X56SMJDT3"
+          strategy="afterInteractive"
+          async
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0X56SMJDT3');
+            `,
+          }}
+        />
+        
         {/* HubSpot Tracking Code */}
         <Script 
           type="text/javascript" 
