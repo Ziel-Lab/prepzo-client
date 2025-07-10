761cb36687d9008a3d5861c0ccf7990784cfb488
Kushagra Gangwar <kgang6434@gmail.com>
2025-07-10 12:35:28 +0530

    updated cover letter company url

--- src/components/dashboard/tools/coverLetter/coverLetterContent.tsx
+++ src/components/dashboard/tools/coverLetter/coverLetterContent.tsx
@@ -136,7 +136,11 @@ const CoverLetterContent = () => {
 
     if (companyWebsiteParam) {
       try {
-        setCompanyWebsite(decodeURIComponent(companyWebsiteParam));
+        let url= decodeURIComponent(companyWebsiteParam);
+        if (!/^https?:\/\//i.test(url)) {
+          url = `https://${url}`;
+        }
+        setCompanyWebsite(url);
       } catch {
         setCompanyWebsite(companyWebsiteParam);
       }
