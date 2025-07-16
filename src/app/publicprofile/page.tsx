a872b778ed219806d964c857f52054dadc193b88
Kushagra Gangwar <kgang6434@gmail.com>
2025-07-16 16:03:44 +0530

    bug-fix

--- src/app/publicprofile/page.tsx
+++ src/app/publicprofile/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
